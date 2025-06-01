217ec9673e8b3c500052c6db19b98ebd97e2c63b
Collins-Ogbodo <117471619+Collins-Ogbodo@users.noreply.github.com>
2025-06-01 17:19:41 +0000

    Deploying to gh-pages from @ Collins-Ogbodo/Collins-Ogbodo.github.io@594d07227c6416d88bcfbb810625241226162345 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-won-the-2025-postgraduate-researcher-experience-programme-prep",
+          title: 'Won the 2025 Postgraduate Researcher Experience Programme (PREP)',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-paper-on-mode-indicator-guided-sequential-modal-analysis-accepted-in-the-international-conference-on-noise-and-vibration-engineering-2024",
+          title: 'Paper on Mode Indicator Guided Sequential Modal Analysis accepted in the International Conference...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
