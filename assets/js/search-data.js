daa134de7bd2aa8a711bba0c11648c5dc34b9065
Collins-Ogbodo <117471619+Collins-Ogbodo@users.noreply.github.com>
2025-06-01 16:03:36 +0000

    Deploying to gh-pages from @ Collins-Ogbodo/Collins-Ogbodo.github.io@827f48770d12e75700a0bfce2a550256d5a712fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,14 +501,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%43%6F%6F%67%62%6F%64%6F%31@%73%68%65%66%66%69%65%6C%64.%61%63.%75%6B", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/Collins-Ogbodo", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/collins-ogbodo-8b163b145", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0002-3672-0240", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,14 +536,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=Collins O Ogbodo", "_blank");
         },
       },{
       id: 'light-theme',
